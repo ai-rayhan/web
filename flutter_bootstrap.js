7bab89da6c723cb0b9d2816541b5c05cbaa84829
ai-rayhan <rayhanahmed1127@gmail.com>
2024-10-29 13:35:25 +0600

    portfolio

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2780939279"
+    serviceWorkerVersion: "1373674735"
   }
 });
